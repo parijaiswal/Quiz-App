f73c173cecff830d24e5748b8e0e6da7f5dbed58
parijaiswal <parijaiswal346@gmail.com>
2025-06-05 22:57:50 +0530

    "Add Score Summary"

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,24 +1,85 @@
 import React from "react";
 import "./Quiz.css";
-import { useState } from "react";
+import { useState,useRef } from "react";
 import {data} from '../../assets/data';
+import ScoreCal from "./ScoreCal";
 const Quiz = () => {
 
   let [index,setIndex] = useState(0);
   let [question,setQuestions] = useState(data[index]);
+  let [lock,setLock] = useState(false);
+  let [score,setScore] = useState(0);
+  let [result,setResult] = useState(false);
+
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null);
+
+  let option_array = [Option1,Option2,Option3,Option4];
+
+  const checkAnswer = (e,ans)=>{
+      if(lock === false)
+      {
+        if(question.ans===ans)
+        {
+        e.target.classList.add("correct");
+        setLock(true);
+        setScore(prev=>prev+1);
+        }
+        else{
+        e.target.classList.add("wrong");
+        setLock(true);
+        option_array[question.ans-1].current.classList.add("correct");
+        }
+      }
+
+  }
+
+  const nextQuestion = ()=>{
+    if(lock===true)
+    {
+        if(index === data.length-1)
+        {
+           setResult(true);
+           return 0;
+        }
+        setIndex(++index);
+        setQuestions(data[index]);
+        setLock(false);
+        option_array.map((option) => {
+            option.current.classList.remove("wrong");
+            option.current.classList.remove("correct");
+            return null;
+        })
+    }
+  }
+
+  const reset = ()=>{
+    setIndex(0);
+    setQuestions(data[0]);
+    setScore(0);
+    setLock(false);
+    setResult(false);
+  }
   return (
     <div className="container">
       <h1>Quiz App</h1>
       <hr />
-      <h2>{index + 1}. {question.question}</h2>
+
+      {result? (
+      <ScoreCal score={score} total={data.length} onReset={reset} />) : (
+        <><h2>{index + 1}. {question.question}</h2>
       <ul>
-        <li>{question.option1}</li>
-        <li>{question.option2}</li>
-        <li>{question.option3}</li>
-        <li>{question.option4}</li>
+        <li ref={Option1} onClick={(e)=>{checkAnswer(e,1)}}>{question.option1}</li>
+        <li ref={Option2} onClick={(e)=>{checkAnswer(e,2)}}>{question.option2}</li>
+        <li ref={Option3} onClick={(e)=>{checkAnswer(e,3)}}>{question.option3}</li>
+        <li ref={Option4} onClick={(e)=>{checkAnswer(e,4)}}>{question.option4}</li>
       </ul>
-      <button>Next</button>
-      <div className="index">1 of 5 questions</div>
+      <button onClick={nextQuestion}>Next</button>
+      <div className="index">{index + 1} of {data.length} questions</div>
+    </>)}
+   
     </div>
   );
 };
---
+++ src/Components/Quiz/ScoreCal.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './ScoreCal.css'
+import { useState } from 'react'
+const ScoreCal = ({score,total, onReset}) => {
+    const percentage = ((score/total)*100).toFixed(2);
+    let remark = "";
+    if(percentage >=85) {
+        remark = "Excellent 🎉";
+    }
+    else if(percentage >= 70){
+        remark = "Good Job😊";
+    }
+    else if(percentage >=50){
+        remark = "Keep Practicing! 💪";
+    }
+  return (
+    <div className="summary">
+        <h2>Quiz Completed!</h2>
+        <p><strong>Total Questions:</strong> {total}</p>
+        <p><strong>Correct Answers: </strong> {score}</p>
+        <p><strong>Score: </strong> {percentage}%</p>
+        <p><strong>Remark: </strong> {remark}</p>
+        <button className="btn" onClick={onReset}>Restart Quiz</button>
+    </div>
+  )
+}
+
+export default ScoreCal
